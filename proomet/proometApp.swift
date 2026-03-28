53c47b04bc51a1ce987b7ea867f990a577b4709a
bent2685 <bent2685@outlook.com>
2026-03-28 19:55:23 +0800

    chore(project): 初始化项目

---
+++ proomet/proometApp.swift
@@ -0,0 +1,17 @@
+//
+//  proometApp.swift
+//  proomet
+//
+//  Created by bent on 2026/3/28.
+//
+
+import SwiftUI
+
+@main
+struct proometApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
